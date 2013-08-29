530fa8ab53f39011980f50a53b6743bc3bfda5ba
KyouheiDegit <kyohoshiko@yahoo.co.jp>
2013-08-30 00:51:03 +0900

    add controller and test

--- js/main.js
+++ js/main.js
@@ -1,14 +1,7 @@
 ;(function($,window,document){
 'use strict';
-function AppUtil(){
-	var instance;
-	AppUtil = function(){ return instance; };
-	AppUtil.prototype = this;
-	instance = new AppUtil();
-	instance.constructor = AppUtil;
-	return instance;
-}
-AppUtil.prototype.getUA = (function(){
+function AppUtil(){}
+AppUtil.prototype.UA = (function(){
 	return (function( ua ){
 		if (~ua.indexOf('msie')) return 'ie';
 		else if (~ua.indexOf('chrome')) return 'chrome';
@@ -17,7 +10,7 @@ AppUtil.prototype.getUA = (function(){
 		else return 'more!!!';
 	}( window.navigator.userAgent.toLowerCase()));
 }());
-AppUtil.prototype.getDeviece = (function(){
+AppUtil.prototype.arrDEVICE = (function(){
 	return (function(ua){
 		if (~ua.indexOf('IPHONE')) return ['sp','iphone'];
 		else if (~ua.indexOf('ANDROID')) return ['sp','android'];
@@ -26,16 +19,34 @@ AppUtil.prototype.getDeviece = (function(){
 		else return ['pc','notSpTab'];
 	})( window.navigator.userAgent.toUpperCase());
 }());
-AppUtil.prototype.msVersion = (function(){
+AppUtil.prototype.MSVERSION = (function(){
 	return (function( uav ){
-		if (AppUtil.prototype.getUA !== 'ie') return 'notIE';
+		if (AppUtil.prototype.UA !== 'ie') return void 0;
 		if (~uav.indexOf("msie 6.")) return 'ie6';
 		else if (~uav.indexOf("msie 7.")) return 'ie7';
 		else if (~uav.indexOf("msie 8.")) return 'ie8';
 		else return 'IEelse';
 	})( window.navigator.appVersion.toLowerCase() );
 }());
-AppUtil.prototype.StyleAdd = function( path ){
+AppUtil.prototype.CLICKTYPE = (function(){
+	return (AppUtil.prototype.arrDEVICE[0] === 'pc') ? 'click' : 'touchstart';
+}());
+AppUtil.prototype.arrQUERY = (function(){
+	return (function(search){
+		if(!search.length) return void 0;
+		return $.map( search.substring( 1 ).split( '&' ) , function(v){
+			return {key:v.split('=')[0],value:v.split('=')[1]};
+		});
+	})(window.location.search);
+}());
+AppUtil.prototype.NAME = (function(){
+	return (function(name){
+		var sName = name.split('/');
+		var tmpName = sName[sName.length - 1 ];
+		return (/./.test(tmpName)) ? tmpName.split('.')[0] : 'index';
+	})(window.location.pathname);
+}());
+AppUtil.prototype.addStyle = function( path ){
 	var link = document.createElement('link');
 	link.rel = 'stylesheet';
 	link.type = 'text/css';
@@ -58,19 +69,8 @@ AppUtil.prototype.loadImage = function( data ){
 		arrImg[i].src = data[i];
 	}
 };
-AppUtil.prototype.clickType = (function(){
-	return (!window. TouchEvent) ? 'click' : 'touchstart';
-}());
-AppUtil.prototype.queryString = (function(){
-	return (function(search){
-		if(!search.length) return void 0;
-		return $.map( search.substring( 1 ).split( '&' ) , function(v){
-			return {key:v.split('=')[0],value:v.split('=')[1]};
-		});
-	})(window.location.search);
-}());
-AppUtil.prototype.setCookie = function(key,value,expiredays){
-	var expiredays = expiredays || 30;
+AppUtil.prototype.setCookie = function(key,value,expireday){
+	var expiredays = expireday || 30;
 	var expires = new Date( new Date().getTime() + (60*60*24*1000*expiredays));
 	document.cookie = key + '=' + encodeURIComponent(value) + '; ' + 'path=/; ' + 'expires=' + expires.toUTCString() + ';';
 };
@@ -79,11 +79,45 @@ AppUtil.prototype.getCookie = function(){
 		return {key:v.split('=')[0],value:v.split('=')[1]};
 	});
 };
+//test
+AppUtil.prototype.domReady = function( callback ){
+	if(document.addEventListener){
+		document.addEventListener('DOMContentLoaded', callback, false);
+	}else if(AppUtil.prototype.UA === 'ie'){
+		try {
+			document.documentElement.doScroll('left');
+		} catch(e){
+			setTimeout(arguments.callee, 0);
+			return;
+		}
+		callback();
+	} else {
+		window.onload = callback;
+	}
+};
+AppUtil.prototype.STORAGE_STATUS = (function(){
+	return (function(storage){
+		return (storage) ? true : false;
+	})(window.sessionStorage);
+}());
+AppUtil.prototype.JSON_BE = (function(){
+	return (function(json){
+		return (typeof json === 'object') ? true : false;
+	})(window.JSON);
+}());
+AppUtil.prototype.setSession = function( key ,value ){
+	if(!AppUtil.prototype.STORAGE_STATUS && !AppUtil.prototype.JSON_BE ) return void 0;
+	window.sessionStorage.setItem( key , window.JSON.stringify( value ));
+};
+AppUtil.prototype.getSession = function( key ){
+	if(!AppUtil.prototype.STORAGE_STATUS && !AppUtil.prototype.JSON_BE ) return void 0;
+	return window.JSON.parse(window.sessionStorage.getItem( key ));
+};
 //======================================================================
 // Base
 //======================================================================
 function AppBase(){}
-AppBase.prototype.ieLog = (function(){
+AppBase.prototype.LOG = (function(){
 	if (!window.console) window.console = { 'log' : function () {} };
 }());
 AppBase.prototype.Resize = function( callback ){
@@ -101,19 +135,40 @@ AppBase.prototype.Orientation = function( callback ){
 AppBase.prototype.Box = (function(){
 	var box = {};
 	return {
-		set : function( key,value ){ box[key] = value; },
-		setList : function( obj ){ Array.prototype.push.call( box , obj );},
+		set : function( key,value ){
+			if(box[key] || box[key] === 0){
+				throw new Error('use '+key+' exist!!');
+			}else{
+				box[key] = value;
+			}
+		},
 		get : function( key ){ return box[key]; },
 		getAll : function(){ return box; },
-		getList : function(){
-			var list = [];
-			$.each( box,function(index,value){
-				if( value !== 'undefind') list.push(value);
-			});
-			return list;
+		update : function( key ,value){
+			if(box[key] || box[key] === 0){
+				delete box[key];
+				box[key] = value;
+			}else{
+				throw new Error('box '+key+' nil!!');
+			}
+		},
+		remove : function(key){
+			delete box[key];
 		}
 	};
 }());
+//======================================================================
+// jQuery
+//======================================================================
+$.fn.srcReplace = function(before,after){
+	var afterAttr = this.attr('src').replace( before , after);
+	var stockImg = new Image();
+	stockImg.onload = $.proxy(function(){
+		this.attr('src', afterAttr);
+		stockImg = null;
+	},this);
+	stockImg.src = afterAttr;
+};
 //======================================================================
 // functions
 //======================================================================
@@ -169,28 +224,34 @@ AppFunctions.prototype.lightboxMake = function( path ){
 		$popUpBlockWrap.fadeIn( fadeTime );
 	});
 };
-AppFunctions.prototype.smooth = function( $target , duration ){
-	var $wrap = $( 'html,body' );
-	duration = duration || 150;
-	$target.on( (!window. TouchEvent) ? 'click' : 'touchstart' , function( e ){
-		e.preventDefault();
-		var href = $( this ).attr('href');
-		$wrap.animate( {scrollTop: ( ( /#/.test(href) ) ? $( href ).offset().top : 0)} , duration );
-	});
-	return $target;
-};
+//======================================================================
+// AppMain
+//======================================================================
 function AppMain(){
 	$.extend(this, new AppBase, new AppUtil);
 }
+AppMain.prototype.test1 = function(){
+	console.log('test1');
+	return this;
+};
+AppMain.prototype.test2 = function(){
+	console.log('test2');
+	return this;
+};
+//======================================================================
+// AppController
+//======================================================================
+function AppController(){
+	this.Main = {};
+	$.extend(this.Main,new AppMain);
+	this[this.Main.NAME]();
+}
+AppController.prototype.index = function(){
+	this.Main.test1().test2();
+};
 //======================================================================
 // Main
 //======================================================================
-var Main;
-$(function(){
-	Main = new AppMain();
-});
-
-$(window).on('load',function(){
-});
+new AppController();
 
 })(jQuery,window,document);
